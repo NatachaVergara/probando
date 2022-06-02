9d6c2bba310e31ac9a4f35b4ab8991a418ca9f86
Natacha Vergara <ntchvergara@gmail.com>
2022-06-02 13:32:37 -0300

    actualizando el todoList

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 // colocá las declaraciones acá
-
+let inputTarea = document.querySelector('#task')
+let inputPrioridad = document.querySelector('#prioridad')
+let ul = document.querySelector('#lista-de-tareas')
 
 
 //declaramos el class Tarea
 
+class Tarea {
+    constructor(nombre, prioridad) {
+        this.nombre = nombre,
+        this.prioridad = prioridad
+    }
+    agregarTareas(array) {
+        listaDeTarea.push(array)
+    }
 
+
+
+}
 
